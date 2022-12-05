3ea6cd53f69e782af7cddbba6d50753de1a7c697
eleonora esau <eleonora.esau@gmail.com>
2022-12-05 15:00:08 +0100

    dropdown menu.02

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import {BiSearch} from 'react-icons/bi'
-import {BsPerson} from 'react-icons/bs'
+import React, {useState} from 'react'
 import {IoMdMenu} from 'react-icons/io'
+import {FaLinkedin, FaInstagram, } from 'react-icons/fa'
+import {ImGithub} from 'react-icons/im'
 
 import './NavbarStyles.css'
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
+
   return (
-    <div className='navbar'>
+    <div className={nav ? 'navbar navbar-bg' : 'navbar'}>
       <div className="logo">
          <h2>ONO</h2>
       </div>
@@ -17,14 +20,14 @@ function Navbar() {
         <li>3D</li>
         <li>ABOUT</li>
       </ul>
-      <div className="nav-icons">
+      {/* <div className="nav-icons">
         <BiSearch className="icon"/>
         <BsPerson className="icon"/>
-      </div>
-      <div className="hamburger">
+      </div> */}
+      <div className="hamburger" onClick={handleNav}>
         <IoMdMenu className="icon"/>
       </div>
-      <div className="mobile-menu">
+      <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
         <ul className="mobile-nav">
           <li>HOME</li>
           <li>&lt;/&gt;</li>
@@ -36,7 +39,9 @@ function Navbar() {
 
           </div>
           <div className="social-icons">
-            
+            <FaInstagram className="icon"/>
+            <FaLinkedin className="icon"/>
+            <ImGithub className="icon"/>
           </div>
         </div>
       </div>
