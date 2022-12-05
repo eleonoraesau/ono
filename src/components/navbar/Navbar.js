a0ab5c6698d73f1a3d0199ad5681c168533ed803
eleonora esau <eleonora.esau@gmail.com>
2022-12-05 11:52:20 +0100

    initialize and navbar.01

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className="logo">
+         <h2>ONO</h2>
+      </div>
+      <ul className="nav-menu">
+        <li>HOME</li>
+        <li>&lt;/&gt;</li>
+        <li>3D</li>
+        <li>ABOUT</li>
+      </ul>
+      <div className="nav-icons">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
