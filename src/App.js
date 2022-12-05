5cec446e494ac69951f6c7e2fb106542543469f8
eleonora esau <eleonora.esau@gmail.com>
2022-12-05 13:35:26 +0100

    media menu update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
-      hey there ono
+      <Navbar />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {BiSearch} from 'react-icons/bi'
+import {BsPerson} from 'react-icons/bs'
+import {IoMdMenu} from 'react-icons/io'
+
+import './NavbarStyles.css'
 
 function Navbar() {
   return (
@@ -13,7 +18,27 @@ function Navbar() {
         <li>ABOUT</li>
       </ul>
       <div className="nav-icons">
-        
+        <BiSearch className="icon"/>
+        <BsPerson className="icon"/>
+      </div>
+      <div className="hamburger">
+        <IoMdMenu className="icon"/>
+      </div>
+      <div className="mobile-menu">
+        <ul className="mobile-nav">
+          <li>HOME</li>
+          <li>&lt;/&gt;</li>
+          <li>3D</li>
+          <li>ABOUT</li>
+        </ul>
+        <div className="mobile-menu-bottom">
+          <div className="menu-icons">
+
+          </div>
+          <div className="social-icons">
+            
+          </div>
+        </div>
       </div>
     </div>
   )
